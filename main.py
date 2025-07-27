b840a1645815b770d8ad4953d3b530446a5e55c8
Noumena Leipon <212272793+noumena-leipon@users.noreply.github.com>
2025-07-26 20:10:05 -0700

    Bookbot complete

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stats import get_book_text, count_words, count_alphas, sort_alphas
 
 
 def main():
-    path = "books/frankenstein.txt"
+    path = filename
     text = get_book_text(path)
     count = count_words(text)
     alphas = count_alphas(text)
@@ -18,5 +18,14 @@ def main():
             print(f"{item['char']}: {item['count']}")
     print("============= END ===============")
 
+
+arguments = sys.argv[0:]
+filename = []
+
+if len(arguments) < 2 or len(arguments) > 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filename = arguments[1]
 
 main()
